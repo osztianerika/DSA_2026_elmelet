049b97a9f7ae0cb99529cffc2d8b9c36d78133b3
osztianerika <osztiane@gmail.com>
2026-03-11 19:21:29 +0200

    feladat kovetelmenye

--- labor_stack_queue/stack_queue/main.c
+++ labor_stack_queue/stack_queue/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    /* Az S verem és a C sor egész számokat tárol. Mindkettőbe betesszük az 1, 2, 3, 4 számokat,
+   ebben a sorrendben. Jelöljük S->C-vel azt a műveletet, amely kivesz egy elemet az S
+   veremből és beteszi a C sorba, és C->S-sel azt a műveletet, amely kivesz egy elemet a C
+   sorból és beteszi az S verembe.
+   Mennyi lesz az S verembe utolsónak betett elem értéke, és mennyi lesz a c sor végére
+   betett utolsó elem értéke, ha a következő műveletsort hajtjuk végre:
+   S->C; C->S; C->S;S->C; C->S;*/
     return 0;
 }
