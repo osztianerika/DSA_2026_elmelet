df474adf5f2ae71c9f1250b7714a426c29c75632
osztianerika <osztiane@gmail.com>
2026-03-11 19:19:38 +0200

    header fajlok tartalma

--- labor_stack_queue/stack_queue/functions_queue.h
+++ labor_stack_queue/stack_queue/functions_queue.h
@@ -4,5 +4,51 @@
 
 #ifndef STACK_QUEUE_FUNCTIONS_QUEUE_H
 #define STACK_QUEUE_FUNCTIONS_QUEUE_H
+typedef struct {
+    int capacity;
+    int front;
+    int rear;
+    int *elements;
+}Queue_t;
+/**
+* Létrehozza a sor adatstruktúrát, memóriát foglal az elemek számára.
+* @param capacity - a sor kapacitása
+* @param queue - pointer a sor struktúrájához
+*/
+void createQueue(int capacity, Queue_t *queue);
 
+/**
+* Felszabadítja az elemek tömbjéhez lefoglalt memóriát.
+* @param queue - pointer a sor struktúrájához
+*/
+void destroyQueue(Queue_t* queue);
+/**
+* Ellenőrzi, hogy a sor tele van-e.
+* @param queue - a sor struktúrája
+* @return igaz, ha tele van, hamis egyébként
+*/
+bool isFullQueue(Queue_t queue);
+/**
+* Ellenőrzi, hogy a sor üres-e.
+* @param queue - a sor struktúrája
+* @return igaz, ha üres, hamis egyébként
+*/
+bool isEmptyQueue(Queue_t queue);
+/**
+* Új elemet ad a sor végéhez (FIFO).
+* @param queue - pointer a sor struktúrájához
+* @param item - a hozzáadandó elem (karakterlánc)
+*/
+void enqueue(Queue_t* queue, int item);
+/**
+* Eltávolítja a sor elején lévő elemet.
+* @param queue - pointer a sor struktúrájához
+* @return a levett elem (karakterlánc)
+*/
+int dequeue(Queue_t* queue);
+/**
+* Kiírja a sor összes elemét.
+* @param queue - a sor struktúrája
+*/
+void display(Queue_t queue);
 #endif //STACK_QUEUE_FUNCTIONS_QUEUE_H
--- labor_stack_queue/stack_queue/functions_stack.h
+++ labor_stack_queue/stack_queue/functions_stack.h
@@ -4,5 +4,55 @@
 
 #ifndef STACK_QUEUE_FUNCTIONS_STACK_H
 #define STACK_QUEUE_FUNCTIONS_STACK_H
+typedef struct {
+    int top;
+    int capacity;
+    int *elements;
+}Stack_t;
+#include<stdio.h>
+#include <stdlib.h>
+#include <constants.h>
+void createStack(int capacity, Stack_t *stack);
+/**
+* Felszabadítja a veremhez lefoglalt memóriát.
+* @param stack - pointer a verem struktúrájához
+*/
+void destroyStack(Stack_t* stack);
 
+/**
+* Ellenőrzi, hogy a verem tele van-e.
+* @param stack - a verem struktúrája
+* @return igaz, ha tele van, hamis egyébként
+*/
+bool isFull(Stack_t stack);
+/**
+* Ellenőrzi, hogy a verem üres-e.
+* @param stack - a verem struktúrája
+* @return igaz, ha üres, hamis egyébként
+*/
+bool isEmpty(Stack_t stack);
+/**
+* Új elemet helyez a verembe, ha nincs tele.
+* @param stack - pointer a verem struktúrájához
+* @param item - a beszúrandó elem
+*/
+void push(Stack_t* stack, int item);
+/**
+* Eltávolítja a verem tetején lévő elemet, ha nem üres.
+* @param stack - pointer a verem struktúrájához
+* @return a levett elem értéke
+*/
+int pop(Stack_t* stack);
+/**
+* Visszaadja a verem tetején lévő elemet, ha nem üres.
+* @param stack - a verem struktúrája
+* @return a tetején lévő elem értéke
+*/
+int peek(Stack_t stack);
+/**
+* Visszaadja a verem méretét.
+* @param stack - a verem struktúrája
+* @return a verem elemeinek száma
+*/
+int size(Stack_t stack);
 #endif //STACK_QUEUE_FUNCTIONS_STACK_H
