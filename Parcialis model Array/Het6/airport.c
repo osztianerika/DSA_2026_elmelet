ae65c565e940bd35142b82f5d88c682706493cc8
osztianerika <osztiane@gmail.com>
2026-03-23 15:44:55 +0200

    7.5 ig

--- Parcialis model Array/Het6/airport.c
+++ Parcialis model Array/Het6/airport.c
@@ -5,36 +5,61 @@
 #include "airport.h"
 
 
-void readFlight(Flight_t* pFlight)
-{
-    scanf("%i\n", &pFlight->flightID);
-    scanf("%[^\n]\n",pFlight->to );
-    scanf("%i", &pFlight->seats);
-    scanf("%d%d%d%d", &pFlight->departureTime.month, &pFlight->departureTime.day, &pFlight->departureTime.hour, &pFlight->departureTime.min);
-    scanf("%i",&pFlight->function_e);
+void readFlight(Flight_t *pFlight) {
+    scanf("%i\n", &pFlight->fligthID);
+    scanf("%[^\n]\n",pFlight->to);
+    scanf("%i",&pFlight->seats);
+    scanf("%d%d%d%d", &pFlight->departureTime.month,&pFlight->departureTime.day,&pFlight->departureTime.hour,&pFlight->departureTime.min);
+    scanf("%i",&pFlight->fligthType);
 }
 
-void printFlight(Flight_t flight)
-{
-    printf("%i ", flight.flightID);
-    printf("%s ",flight.to);
-    printf("%i ", flight.seats);
-    printf("%d %d %d %d ", flight.departureTime.month, flight.departureTime.day, flight.departureTime.hour, flight.departureTime.min);
-    printf("%s ", getDescriptionFlightFunction(flight.function_e));
+void printFlight(Flight_t pFlight_t) {
+    printf("%i ", pFlight_t.fligthID);
+    printf("%s ",pFlight_t.to);
+    printf("%i ",pFlight_t.seats);
+    printf("%d %d %d %d ", pFlight_t.departureTime.month,pFlight_t.departureTime.day,pFlight_t.departureTime.hour,pFlight_t.departureTime.min);
+    printf("%i ",pFlight_t.fligthType);
     printf("\n");
 }
-
-char* getDescriptionFlightFunction(enum FlightType flight)
-{
-    switch (flight)
-    {
-    case 0 : return "UTAS";
-    case 1 : return "TEHER";
-    case 2 : return "MAGAN";
-        default : return "MAS";
+void createIntArray(int capacity, FlightArray_t *pArray) {
+    pArray->capacity = capacity;
+    pArray->size = 0;
+    pArray->elements = (Flight_t*)calloc(pArray->capacity, sizeof (Flight_t));
+    if(!pArray->elements) {
+        printf("MEMORY_ALLOCATION_ERROR_MESSAGE");
+        exit(-2);
     }
 }
-
-
-
-
+bool isFull(FlightArray_t array) {
+    return array.size == array.capacity;
+}
+bool isEmpty(FlightArray_t array) {
+    return array.size == 0;
+}
+void printArray(FlightArray_t array) {
+    if(isEmpty(array)) {
+        printf("EMPTY_MESSAGE");
+        return;
+    }
+    printf("The elements of the array:");
+    for (int i = 0; i < array.size; ++i) {
+        printFlight(array.elements[i]);
+    }
+    printf("\n");
+}
+void insertFirst(FlightArray_t *pArray, Flight_t item) {
+    if(isFull(*pArray)) {
+        printf("FULL_MESSAGE");
+        return;
+    }
+    for (int i = pArray->size; i > 0; --i) {
+        pArray->elements[i] = pArray->elements[i-1];
+    }
+    pArray->elements[0] = item;
+    pArray->size++;
+}
+void deallocateIntArray(FlightArray_t *pArray) {
+    free(pArray->elements);
+    pArray->capacity = pArray->size = 0;
+    pArray = NULL;
+}
--- Parcialis model Array/Het6/main.c
+++ Parcialis model Array/Het6/main.c
@@ -2,25 +2,29 @@
 #include <constants.h>
 #include <stdlib.h>
 
-int main(void)
-{
-    // Flight_t flight1,flight2;
-    // readFlight(&flight1);
-    // printFlight(flight1);
-    //
-    // if (!freopen("flight_one.txt", "r", stdin))
-    // {
-    //     printf(FILE_OPENING_ERROR_MESSAGE);
-    //     exit (-1);
-    // }
-    // readFlight(&flight2);
-    // printFlight(flight2);
-    //
-    // Flight_t flight3 ={
-    //     102, "Targu mures", 105, {03, 23, 11, 50},
-    // 0};
-    // printFlight(flight3);
-
-
+int main(void) {
+    /*Flight_t flight1,flight2;
+    readFlight(&flight1);
+    freopen("eredmeny_repulok.txt","w",stdout);
+    printFlight(flight1);
+    if (!freopen("flight_one.txt","r",stdin)) {
+        printf("FAILED TO ACCES DIRECTORY");
+        return -1;
+    }
+    readFlight(&flight2);
+    printFlight(flight2);*/
+    int n;
+    Flight_t fligth;
+    FlightArray_t array;
+    createFlightArray(100,&array);
+    if (!freopen("flights.txt","r",stdin)) {
+        printf("ERROR");
+        return -1;
+    }
+    scanf("%i",&n);
+    for (int i=0;i<n;i++) {
+        readFlight(&fligth);
+        insertFirst(&array,fligth);
+    }
     return 0;
 }
